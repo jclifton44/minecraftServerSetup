468e5de96e62d4ffb0fe81faff053fcd598681f4
root <root@jeremy-clifton.com>
2020-04-12 19:37:00 +0000

    setupServer

--- setupServer.sh
+++ setupServer.sh
@@ -1,14 +1,24 @@
 apt-get install make gcc
 apt-get install unzip
 #update to ssl 1.1.1
-wget 'https://www.openssl.org/source/openssl-1.1.1f.tar.gz'
+SSLName="openssl-1.1.1f.tar.gz"
+wget "https://www.openssl.org/source/$SSLName.sha256"
+wget "https://www.openssl.org/source/$SSLName"
+
+echo "$(cat ${SSLName}.sha256) $SSLName" | sha256sum --check
+if [ $? -ne 0 ];
+then
+	echo "Checksums do not match. Exiting..."
+	exit
+fi
+exit
 tar -zxf 'openssl-1.1.1f.tar.gz'
 cd openssl-1.1.1f
 ./config
 make
 make test
 mkdir ~/tmp
-sudo mv	/usr/bin/openssl ~/tmp
+cp -r /usr/bin/openssl ~/tmp
 make install
 ln -s /usr/local/bin/openssl /usr/bin/openssl
 ldconfig
