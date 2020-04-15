cc618c3163c1b0466c77c39f141264f8a94a90a6
root <root@jeremy-clifton.com>
2020-04-15 23:18:46 +0000

    backup system update

--- checkServicesMC.sh
+++ checkServicesMC.sh
@@ -1,5 +1,6 @@
 cd /opt/minecraftServer/bedrock-server
 DATE_EPOCH=$(date +%s)
+BACKUP_DIR="/opt/minecraftServer/mcWorldData/"
 ps ax | grep "bedrock_server" | grep bedrock-server
 if [ $? -ne '0' ];
 then
@@ -8,8 +9,10 @@ then
 else 
 	#server running
 	PID_VALUE=$(ps -ax | grep bedrock_server | awk {'print$1'} | head -n 1)
+	mkdir ${BACKUP_DIR}${PID_VALUE}
+	rm -rf ${BACKUP_DIR}${PID_VALUE}/*
 	tar -czvf ${DATE_EPOCH}_${PID_VALUE}MCworlds.tar /opt/minecraftServer/bedrock-server/worlds
-	cp ${DATE_EPOCH}_${PID_VALUE}MCworlds.tar /var/www/html/hosted/
+	cp ${DATE_EPOCH}_${PID_VALUE}MCworlds.tar ${BACKUP_DIR}${PID_VALUE}/
 fi
 exit
 
--- setupServerMC.sh
+++ setupServerMC.sh
@@ -1,10 +1,14 @@
 wget 'https://minecraft.azureedge.net/bin-linux/bedrock-server-1.14.32.1.zip'
+HOSTED_BACKUP="/var/www/html/hosted/"
 mkdir /opt/minecraftServer
+cd /opt/minecraftServer
 mkdir bedrock-server
+mkdir /mcWorldData
 cp bedrock-server-1.14.32.1.zip bedrock-server/
 cd bedrock-server
 unzip 'bedrock-server-1.14.32.1.zip'
 cd ../
+ln -s /opt/minecraftServer/mcWorldData ${HOSTED_BACKUP}
 cp -r bedrock-server /opt/minecraftServer/
 cp checkServicesMC.sh /usr/bin/checkServicesMC
 cat /etc/crontab | grep checkServicesMC
