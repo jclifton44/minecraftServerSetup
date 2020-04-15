ef0b007178b375093b63c2c974e172ec7097eb16
root <root@jeremy-clifton.com>
2020-04-15 17:47:08 +0000

    backups

--- checkServicesMC.sh
+++ checkServicesMC.sh
@@ -1,11 +1,15 @@
-cd /opt/minecraftServer/bedrock
-tar -czvf MCworlds.tar /opt/minecraftServer/bedrock-server/
-cp MCworlds.tar /var/www/html/hosted/
-ps ax | grep "bedrock_server" | grep bin
+cd /opt/minecraftServer/bedrock-server
+DATE_EPOCH=$(date +%s)
+ps ax | grep "bedrock_server" | grep bedrock-server
 if [ $? -ne '0' ];
 then
 	cd /opt/minecraftServer/bedrock-server
 	./bedrock_server&
+else 
+	#server running
+	PID_VALUE=$(ps -ax | grep bedrock_server | awk {'print$1'} | head -n 1)
+	tar -czvf ${DATE_EPOCH}_${PID_VALUE}MCworlds.tar /opt/minecraftServer/bedrock-server/worlds
+	cp ${DATE_EPOCH}_${PID_VALUE}MCworlds.tar /var/www/html/hosted/
 fi
 exit
 
