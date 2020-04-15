a3e1857b15d44135602b28c84e81b8c930c89bab
root <root@jeremy-clifton.com>
2020-04-15 17:18:13 +0000

    changed backup strategy

--- checkServicesMC.sh
+++ checkServicesMC.sh
@@ -1,3 +1,6 @@
+cd /opt/minecraftServer/bedrock
+tar -czvf MCworlds.tar /opt/minecraftServer/bedrock-server/
+cp MCworlds.tar /var/www/html/hosted/
 ps ax | grep "bedrock_server" | grep bin
 if [ $? -ne '0' ];
 then
