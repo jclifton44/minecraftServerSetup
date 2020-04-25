7789f3f3a5cdcaae4d6980427b95d8fdd062c2d8
root <root@jeremy-clifton.com>
2020-04-25 15:07:16 +0000

    recent

--- checkServicesMC.sh
+++ checkServicesMC.sh
@@ -2,6 +2,7 @@ cd /opt/minecraftServer/bedrock-server
 DATE_EPOCH=$(date +%s)
 BACKUP_DIR="/opt/minecraftServer/mcWorldData/"
 mkdir ${BACKUP_DIR}/latest/
+mkdir ${BACKUP_DIR}/recent/
 echo $?
 echo "VALUE"
 ps ax | grep "bedrock_server" | grep -v grep
@@ -15,15 +16,15 @@ then
 else 
 	#server running
 	PID_VALUE=$(ps -ax | grep bedrock_server | awk {'print$1'} | head -n 1)
-	mkdir ${BACKUP_DIR}${PID_VALUE}
-	rm -rf ${BACKUP_DIR}${PID_VALUE}/*
+	mkdir ${BACKUP_DIR}recent/${PID_VALUE}
+	rm -rf ${BACKUP_DIR}recent/${PID_VALUE}/*
 	rm -rf ${BACKUP_DIR}latest/*
 	tar -czvf ${DATE_EPOCH}_${PID_VALUE}MCworlds.tar /opt/minecraftServer/bedrock-server/worlds
-	cp ${DATE_EPOCH}_${PID_VALUE}MCworlds.tar ${BACKUP_DIR}${PID_VALUE}/
+	cp ${DATE_EPOCH}_${PID_VALUE}MCworlds.tar ${BACKUP_DIR}recent/${PID_VALUE}/
 	cp ${DATE_EPOCH}_${PID_VALUE}MCworlds.tar ${BACKUP_DIR}latest/
 	rm ${DATE_EPOCH}_${PID_VALUE}MCworlds.tar
-	cd ${BACKUP_DIR}
-	find ${BACKUP_DIR} -mtime +0 -exec rm -rf {} \;
+	cd ${BACKUP_DIR}recent/
+	find ${BACKUP_DIR}recent/ -mtime +0 -exec rm -rf {} \;
 
 fi
 exit
