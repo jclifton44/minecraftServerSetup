32e40e24315b2f8aa4b6293bdd64019f8c2a637f
BuildTools <unconfigured@null.spigotmc.org>
2020-04-27 04:40:51 +0000

    spigot

---
+++ checkServicesMCJavaSpigot1.sh
@@ -0,0 +1,38 @@
+cd /opt/minecraftServerJavaSpigot/
+DATE_EPOCH=$(date +%s)
+BACKUP_DIR="/opt/minecraftServerJavaSpigot/mcWorldData/"
+mkdir ${BACKUP_DIR}/latest/
+mkdir ${BACKUP_DIR}/recent/
+echo $?
+ps ax | grep "server.jar" | grep -v grep
+if [ "$?" -ne 0 ];
+then
+	echo "NOT RUNNING."
+	echo "STARTING..."
+	cd /opt/minecraftServerJavaSpigot/
+	java -Xmx1024M -Xms1024M -jar spigot*.jar nogui
+	echo ${DATE_EPOCH} >> uptime
+else 
+	#server running
+	PID_VALUE=$(ps -ax | grep bedrock_server | awk {'print$1'} | head -n 1)
+	mkdir mcWorld
+	mkdir /mcWorld/recent
+	mkdir /mcWorld/latest
+	mkdir /mcWorld/monthly
+	mkdir ${BACKUP_DIR}recent/${PID_VALUE}
+	rm -rf ${BACKUP_DIR}recent/${PID_VALUE}/*
+	rm -rf ${BACKUP_DIR}latest/*
+	rm -rf mcWorld/*
+	cp -r DIM* /mcWorld
+	cp -r level.dat /mcWorld
+	cp -r region /mcWorld
+	tar -czvf ${DATE_EPOCH}_${PID_VALUE}MCworlds.tar mcWorld
+	cp ${DATE_EPOCH}_${PID_VALUE}MCworlds.tar ${BACKUP_DIR}recent/${PID_VALUE}/
+	cp ${DATE_EPOCH}_${PID_VALUE}MCworlds.tar ${BACKUP_DIR}latest/
+	rm ${DATE_EPOCH}_${PID_VALUE}MCworlds.tar
+	cd ${BACKUP_DIR}recent/
+	find ${BACKUP_DIR}recent/ -mtime +0 -exec rm -rf {} \;
+
+fi
+exit
+
---
+++ setupServerMCJavaSpigot.sh
@@ -0,0 +1,30 @@
+apt-get install default-jre
+PORT_NUMBER="234"
+mkdir /opt/minecraftServerJavaSpigot
+cp checkServicesMCJavaSpigot.sh /usr/bin/checkServicesMCJavaSpigot
+cp dailyRestoreMCJavaSpigot.sh /usr/bin/dailyRestoreMCJavaSpigot
+cd /opt/minecraftServerJavaSpigot
+wget "https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar"
+java -Xmx1G -Xms1G -jar BuildTools.jar
+HOSTED_BACKUP="/var/www/html/hosted/"
+mkdir /mcWorldData
+cd ../
+ln -s /opt/minecraftServerJavaSpigot/mcWorldData ${HOSTED_BACKUP}
+#ufw allow ${PORT_NUMBER}
+#ufw disable
+#ufw enable
+cat /etc/crontab | grep checkServicesMCJavaSpigot
+if [ $? -ne '0' ];
+then
+echo "*/5 * * * *       root    checkServicesMCJavaSpigot" >> /etc/crontab
+fi
+
+
+
+cat /etc/crontab | grep dailyRestoreMCJavaSpigot
+if [ $? -ne '0' ];
+then
+echo "0 8 * * *       root    dailyRestoreMCJavaSpigot" >> /etc/crontab
+fi
+
+
